00611cecefa406978ae17a2e850ce92e3286e735
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-12-09 00:10:32 -0300

    :feat: creating Header component

--- src/app/partials/Header/Header.tsx
+++ src/app/partials/Header/Header.tsx
@@ -1,7 +1,56 @@
+import Image from 'next/image';
+import { ChevronDownIcon, UserIcon } from 'lucide-react';
+import './Header.scss';
+
+import BankLogo from '@/app/assets/union_logo.png';
+
 export const Header = () => {
     return (
-        <header>
-            
+        <header className='header'>
+            <article className='header-navigation'>
+                <Image
+                    alt='Is this image Tesla Bank logo.'
+                    src={BankLogo} 
+                    className='logo-image'
+                />
+
+                <nav className='navbar'>
+                    <ul className='list'>
+                        <li>
+                            <h2>Tesla Bank</h2>    
+                            <ChevronDownIcon
+                                width={22}
+                                color='#EB3850'
+                                className='icon' 
+                            />
+                        </li>
+                        <li>
+                            <h2>Conta Digital</h2>
+                            <ChevronDownIcon
+                                width={22}
+                                color='#EB3850'
+                                className='icon' 
+                            />
+                        </li>
+                        <li>
+                            <h2>Para Você</h2>
+                            <ChevronDownIcon
+                                width={22}
+                                color='#EB3850'
+                                className='icon' 
+                            />
+                        </li>
+                    </ul>
+                </nav>
+            </article>
+
+            <article className='header-login'>
+                <h2>Login</h2>
+                <UserIcon
+                    width={20}
+                    color='#FFF' 
+                />
+            </article>
         </header>
     )
 }
